2c1c7804ca2158fa0b7c81b23b4b64081cc95a6e
Guillermo Federico Olmedo <guillermo.olmedo@gmail.com>
2013-03-11 15:21:27 -0300

    errores y agregado gráficos
    Errores: que ordene por LS; En los IDs el error de "subindice fuera de rango".
    Agregado la capacidad de greficar!

--- Depth_functions.R
+++ Depth_functions.R
@@ -13,7 +13,7 @@ VarName = "CO"			 #Nombre de la variable (se usa para las columnas, y el archivo
 ##### Parametros de los splines ##################################################################
 
 dat2<-read.table (FileName, sep=",", header=TRUE)
-dat2 <- dat2[order(dat2[cID]), ]
+dat2 <- dat2[order(dat2[cID],dat2[cLS]), ] 
 IDnames <- as.character(unique.default(dat2[,cID]))
 
 ndata=length(unique.default(dat2[,cID]))		
@@ -21,22 +21,17 @@ lam <- 0.5 					 #Lambda value  aumentar el valor para hacerlo mas duro
 mxd <- 100 					 #max depth for spline 
 d<- t(c(0,5,15,30,100)) 			 #GlobalSoilMap.Net specifications or user defined depths
 
-
-###########################################################################################################
-############# ACA EMPIEZA EL CODIGO PURO, no deberia haber necesidad de cambiar nada ######################
-###########################################################################################################
 # Requiere paquete Splines
 library(splines)
 load("spline_functions.RData") #load all functions
 # Inputs
-dat2
 dat_x<-cbind(dat2[,cID], dat2[,cLS], dat2[,cLI], dat2[,cAttrib])  
 names (dat_x)
 ###Function for changing alphabet codes to numeric codes | Muy bueno esta
-levels(dat_x[,1]) <- c(1:length(levels(dat_x[,1])))
+dat_x[,1]<-as.factor(dat_x[,1])
 dat_m<- as.matrix(dat_x)
 dat_m<-alp_num(dat_m)
-dat_m
+
 ############################################################################## 
 # Fit splines to raw profile data
 #dat_m<- as.matrix(dat)
@@ -45,6 +40,22 @@ int_s<-ea_spline(dat_m,ndata,lam,d)
 nyfit<-int_s[[1]]
 spfit<-int_s[[2]]
 nyfit
+nyfit[nyfit<0] <- NA
+spfit[spfit<0] <- NA
+
+#### Graficos  #####
+#for (st in 1:ndata) {
+st=sample(1:ndata, 1)
+subs <- subset(dat_m, dat_m[, 1] == st)
+plot(c(subs[,4][1], subs[,4]), c(0,subs[,3]), type="s", xlab=VarName, ylab="Profundidad", ylim = rev(range(c(0,subs[,3]))))
+lines(spfit[,st], 0:mxd,col="red", lty=2, ylim = rev(range(c(0,subs[,3]))))
+abline(h=as.vector(d), lty=2, col="grey")
+newhor <- vector()
+for(nwh in 1:length(d)-1){newhor[nwh] <- (d[nwh]+d[nwh+1])/2}
+points(nyfit[st,1:length(d)-1],newhor, col="blue", pch=4,ylim = rev(range(c(0,subs[,3]))))
+legend(x=max(c(subs[,4][1], subs[,4]))*0.8, y=max(c(0,subs[,3]))*0.7, legend= c("medido", "spline", "horiz. sintéticos", "muestreo"), col=c("black", "red", "grey", "blue"),text.col = "black", lty = c(1, 2, 2, 0), pch = c(NA, NA, NA, 4))
+
+#### Exportar ####
 cNames <- c("ID", paste(VarName, paste(d[,1:length(d)-1], "-", d[,2:length(d)], sep=""), sep="_"), "MaxDepth")
 cNames
 OutFileName <- paste(substr(FileName, 1, nchar(FileName)-4), "_", VarName, ".csv", sep="")

88839c72e9210c08499f1ce12dc07618f91e8bf9
Guillermo Federico Olmedo <guillermo.olmedo@gmail.com>
2013-07-19 17:00:51 -0300

    agregado trasnformacion a raster y plot

--- textura.R
+++ textura.R
@@ -1,15 +1,21 @@
-library(soiltexture)
 library(raster)
+library(soiltexture)
+
 
 ## Cargamos las capas necesarias
 SAND <- raster("RK/resultados/ARE_5.15_RK.tif")
 SILT <- raster("RK/resultados/LIM_5.15_RK.tif")
 CLAY <- raster("RK/resultados/ARC_5.15_RK.tif")
-
-textura.df <- as.data.frame(cbind(na.omit(values(SAND)),na.omit(values(SILT)),na.omit(values(CLAY))))
+textura.df <- as.data.frame(cbind(values(SAND),values(SILT),values(CLAY)))
 names(textura.df) <- c("SAND","SILT","CLAY")
 textura.df <- TT.normalise.sum(tri.data=textura.df,tri.pos.tst=FALSE)
-textura <-   TT.points.in.classes(tri.data=textura.df, class.sys="USDA.TT", PiC.type="t", tri.pos.tst=F)
-textura <- as.factor(textura)
+completos <- !is.na(textura.df)[,1]
+incompletos <- is.na(textura.df)[,1]
+textura.df$CLASS[completos] <-  TT.points.in.classes(tri.data=textura.df[completos,], class.sys="USDA.TT", PiC.type="t", tri.pos.tst=F)
+textura.df$CLASS <- as.factor(textura.df$CLASS)
+table(textura.df$CLASS)
+clases <- levels(textura.df$CLASS)
 
-table(textura)
+## Y obtenemos el raster
+textura.r <- setValues(clasificacion, as.numeric(textura.df$CLASS))
+plot(textura.r)
